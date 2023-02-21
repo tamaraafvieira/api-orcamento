34fdaecc2941ea86a17232c41a805a56e359e6c5
= <tamara.afvieira@gmail.com>
2023-02-21 20:53:03 -0300

    Busca de usuários e produtos por id

--- index.js
+++ index.js
@@ -3,45 +3,63 @@ const axios= require('axios')
 const express = require('express')
 const { response } = require("express")
 const server = express()
-// const app = express()
-
-// const getUsers = require('./src/routes/users');
-// const getProducts = require('./src/routes/products');
-// const calculateBudget = require('./src/routes/budget');
-
 
 server.use(express.json())
 
 server.listen(8080)
 
-    //Esse funciona, mas é passando localmente
-// server.get("/", (req, res) => {
-//   return res.send({
-//     message: "Tamara"
-//   })
-// })
 
-
-      //Esse funciona, pegando da api, retorna passando o id no navegador
-// server.get('/products/:id', async (req, res) => {
-//   const {id} = req.params
-// try{
-//   const {data} = await api.get(`products/${id}`)
-//   return res.send({ id: data.id })
-// } catch (error) {
-//   res.send({ error: error.message })
-// }
-// })
-
-server.get('/products', async (req, res) => {
+// Adicionando os usuários em um Array 
+async function usersArray() {
   try {
-    const { data } = await api.get('/products')
-    return res.send(data)
+    const response = await api.get('/users')
+    const users = response.data
+
+    const usersArray = []
+
+    users.forEach(user => {
+      usersArray.push(user)
+    })
+
+    console.log(usersArray)
+    return usersArray
   } catch (error) {
-    return res.status(500).send({ error: error.message })
+    console.error({ error: error.message })
   }
+}
+
+let userList = []
+
+server.on("listening", async () => {
+  userList = await usersArray();
 })
 
+// Adicionando os produtos em um Array
+async function productsArray() {
+  try {
+    const response = await api.get('/products')
+    const products = response.data
+    const productsArray = []
+
+    products.forEach(product => {
+      productsArray.push(product)
+    });
+
+    console.log(productsArray)
+    return productsArray
+  } catch (error) {
+    console.error({ error: error.message })
+  }
+}
+
+let productList = []
+
+server.on("listening", async () => {
+  productList = await productsArray();
+})
+
+
+//Traz a lista completa de todos os usuários cadastrados 
 server.get('/users', async (req, res) => {
   try {
     const { data } = await api.get('/users')
@@ -51,20 +69,80 @@ server.get('/users', async (req, res) => {
   }
 })
 
+//Traz a lista completa de todos os produtos cadastrados 
+server.get('/products', async (req, res) => {
+  try {
+    const { data } = await api.get('/products')
+    return res.send(data)
+  } catch (error) {
+    return res.status(500).send({ error: error.message })
+  }
+})
 
 
-// app.get('/users', async (req, res) => {
-//     const users = await getUsers()
-//     res.send(users)
+
+//Retorna o usuário pelo id
+async function getUserById(userId) {
+  try {
+    const userList = await usersArray()
+    const user = userList.find(user => Number(user.id) === Number(userId))
+
+    if (!user) {
+      return { error: "User not found" }
+    }
+
+    return user
+  } catch (error) {
+    console.error(error)
+    return { error: error.message }
+  }
+}
+
+server.get('/user/:id', async (req, res) => {
+  const userId = req.params.id
+  const user = await getUserById(userId)
+
+  return res.json(user)
+})
+
+//Retorna o produto pelo id
+async function getProductById(productId) {
+  try {
+    const productList = await productsArray()
+    const product = productList.find(product => Number(product.id) === Number(productId))
+
+    if (!product) {
+      return { error: "Product not found" }
+    }
+
+    return product
+  } catch (error) {
+    console.error(error)
+    return { error: error.message }
+  }
+}
+
+server.get('/product/:id', async (req, res) => {
+  const productId = req.params.id
+  const product = await getProductById(productId)
+
+  return res.json(product)
+})
+
+
+
+
+
+//Retorna passando o id no navegador, porém retorna somente o id, sem as informações , está funcionando
+// server.get('/products/:id', async (req, res) => {
+//   const {id} = req.params
+// try{
+//   const {data} = await api.get(`products/${id}`)
+//   return res.send({ id: data.id })
+// } catch (error) {
+//   res.send({ error: error.message })
+// }
 // })
-
-
-// app.use(async (req, res, next) => {
-//   const budget = await calculateBudget();
-//   res.locals.budget = budget;
-//   next();
-// });
-
 
 
 
